4c1b1c98f54449f18a79e669ee2a66991f628c6a
hongrae <hongrae24@gmail.com>
2024-09-24 10:40:05 +0900

    달력 꾸미기

--- diary/views/main_view.py
+++ diary/views/main_view.py
@@ -14,15 +14,23 @@ def home():
 @bp.route("/<int:year>/<int:month>")
 def calendar(year, month):
     if month < 1 or 12 < month:
-        return redirect("/invalid_date")
-    dates = []
-    last = monthrange(year, month)[1]
+        month -= 1
+        year += month // 12
+        month %= 12
+        month += 1
+        return redirect("/{0}/{1}".format(year, month))
+    dates = [] # 0 is invalid, 1 is valid but has no data, 2 has data 
+    first, last = monthrange(year, month)
+    for i in range(first):
+        dates.append((0, False))
     for i in range(1, last + 1):
         d = date(year, month, i)
         if Diary.query.get(d) is None:
-            dates.append(False)
+            dates.append((i, False))
         else:
-            dates.append(True)
+            dates.append((i, True))
+    while len(dates) % 7:
+        dates.append((0, False))
     return render_template("cal.html", year=year, month=month, dates=dates)
 
 @bp.route("/<int:year>/<int:month>/<int:day>")
@@ -30,7 +38,6 @@ def content(year, month, day):
     try:
         d = date(year, month, day)
     except ValueError:
-        print(day)
         return redirect("/invalid_date")
     diary = Diary.query.get_or_404(d)
     return render_template("diary.html", diary=diary)
