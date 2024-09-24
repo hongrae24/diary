208283b987e87d70070ecacfe2a6d38d2bc85c6e
hongrae <hongrae24@gmail.com>
2024-09-24 09:00:07 +0900

    기본 달력에 일기 링크 추가

--- diary/views/main_view.py
+++ diary/views/main_view.py
@@ -1,20 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from diary import Diary
 
-from datetime import datetime
+from datetime import datetime, date
+from calendar import monthrange
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 @bp.route("/")
 def home():
-    diary_list = Diary.query.all()
-    return render_template("main.html", diary_list=diary_list)
+    t = datetime.today()
+    return redirect("/{0}/{1}".format(t.year, t.month))
 
-@bp.route("/<date>")
-def content(date):
+@bp.route("/<int:year>/<int:month>")
+def calendar(year, month):
+    if month < 1 or 12 < month:
+        return redirect("/invalid_date")
+    dates = []
+    last = monthrange(year, month)[1]
+    for i in range(1, last + 1):
+        d = date(year, month, i)
+        if Diary.query.get(d) is None:
+            dates.append(False)
+        else:
+            dates.append(True)
+    return render_template("cal.html", year=year, month=month, dates=dates)
+
+@bp.route("/<int:year>/<int:month>/<int:day>")
+def content(year, month, day):
     try:
-        date = datetime.strptime(date, "%Y-%m-%d").date()
+        d = date(year, month, day)
     except ValueError:
-        pass
-    diary = Diary.query.get_or_404(date)
+        print(day)
+        return redirect("/invalid_date")
+    diary = Diary.query.get_or_404(d)
     return render_template("diary.html", diary=diary)
+
+# TODO: make button to go today
+@bp.route("/invalid_date")
+def invalid_date():
+    return "Invalid date"
